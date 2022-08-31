84d3c9470dcc84419dd3fbd1bdd5d9a1a233a57e
tuanit168 <tuanit168@gmail.com>
2022-08-31 18:05:24 +0700

    06 - Provide some comments

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -1,3 +1,4 @@
+// In order to write the test, We have to setup the Connection and the Queries object first (to do that inside this file)
 package db
 
 import (
@@ -14,18 +15,21 @@ const (
 	dbSource = "postgresql://root:12345678@localhost:5432/simple_bank?sslmode=disable"
 )
 
-var testQueries *Queries
+var testQueries *Queries // Will define a testQueries object(contains a DBTX which is db conn or Tx) as a global variable
 
 /*
- * The main entry point of all unit tests inside 1 specific Golang package (db)
+ * The main entry point of all unit tests inside 1 specific Golang package (package db)
  */
+
 func TestMain(m *testing.M) {
-	conn, err := sql.Open(dbDriver, dbSource)
+	conn, err := sql.Open(dbDriver, dbSource) // Create a new connection to the DB, returns a connection obj and an Error
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
 
-	testQueries = New(conn)
+	testQueries = New(conn) // Use a connection to create a new testQueries object
 
+	// m.Run(): To start running the Unit test which will return an 'exit code'(test pass or fail)
+	// Then we should report it back to the test runner via os.Exit() command.
 	os.Exit(m.Run())
 }
